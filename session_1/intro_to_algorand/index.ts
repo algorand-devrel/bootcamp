4b8447496798472010c4de238e7c5a3e2f430f03
Joe Polny <joepolny@gmail.com>
2023-04-05 07:51:50 -0400

    add comments

--- session_1/intro_to_algorand/index.ts
+++ session_1/intro_to_algorand/index.ts
@@ -21,6 +21,8 @@ async function main() {
   console.log('Mnemonic:', algosdk.secretKeyToMnemonic(account.sk));
   console.log('Address:', account.addr);
 
+  await waitForInput();
+
   // Create connection to network via public algod API
   const algodToken = '';
   const algodServer = 'https://testnet-api.algonode.cloud';
@@ -29,6 +31,10 @@ async function main() {
 
   // get accountInfo
   let accountInfo = await algodClient.accountInformation(account.addr).do();
+
+  console.log('accountInfo:', accountInfo);
+
+  await waitForInput();
 
   console.log('Dispsene ALGO at https://testnet.algoexplorer.io/dispenser. Script will continue once ALGO is received...');
 
@@ -47,24 +53,50 @@ async function main() {
 
   console.log(`${account.addr} funded!`);
 
+  await waitForInput();
+
+  // Get basic information needed for every transcation
   const suggestedParams = await algodClient.getTransactionParams().do();
+  console.log('suggestedParams:', suggestedParams);
+
+  await waitForInput();
+
+  // First transaction: payment
+  const dispenserAddress = 'DISPE57MNLYKOMOK3H5IMBAYOYW3YL2CSI6MDOG3RDXSMET35DG4W6SOTI';
+
+  const paymentTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    suggestedParams,
+    from: account.addr,
+    to: dispenserAddress,
+    amount: 0.5 * 1e6, // * 1e6 to convert from ALGO to microALGO
+  });
+
+  const signedPaymentTxn = paymentTxn.signTxn(account.sk);
+
+  await algodClient.sendRawTransaction(signedPaymentTxn).do();
+  console.log(`Sending payment transaction ${paymentTxn.txID()}...`);
 
   const roundsToWait = 3;
+  await algosdk.waitForConfirmation(algodClient, paymentTxn.txID(), roundsToWait);
+
+  console.log(`Payment transaction ${paymentTxn.txID()} confirmed! See it at https://testnet.algoscan.app/tx/${paymentTxn.txID()}`);
+
+  await waitForInput();
 
   // Create token
   const assetCreateTxn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
     suggestedParams,
-    from: account.addr,
-    assetName: 'Matthew Asset',
-    unitName: 'MA',
-    total: 100,
-    decimals: 1,
-    reserve: account.addr,
-    defaultFrozen: false,
-    freeze: account.addr,
-    clawback: account.addr,
-    assetURL: 'https://developer.algorand.org',
-    manager: account.addr,
+    from: account.addr, // The account that will create the asset
+    assetName: 'My First Asset', // The name of the asset
+    unitName: 'MFA', // The short name of the asset
+    total: 100, // The total amount of the smallest unit of the asset
+    decimals: 1, // The number of decimals in the asset
+    reserve: account.addr, // The address of the account that holds the uncirculated/unminted supply of the asset
+    freeze: account.addr, // The address of the account that can freeze or unfreeze the asset in a specific account
+    defaultFrozen: false, // Whether or not the asset is frozen by default
+    clawback: account.addr, // The address of the account that can clawback the asset
+    assetURL: 'https://developer.algorand.org', // The URL where more information about the asset can be retrieved
+    manager: account.addr, // The address of the account that can change the reserve, freeze, clawback, and manager addresses
   });
 
   const signedAssetCreateTxn = assetCreateTxn.signTxn(account.sk);
@@ -104,13 +136,17 @@ async function main() {
 
   await waitForInput();
 
-  // Atomic transactions
+  // Use an atomic transaction group to successfully transfer the asset
 
-  // Fund account, opt in, and then transfer asset
+  // Fund new account
+  // The account needs 0.2 ALGO to receiver an Asset
+  // 0.1 ALGO is the minimum amount of ALGO needed to be included in the ledger
+  // An additional 0.1 ALGO is needed for every asset an account holds, to rent the space in the ledger
   const fundTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
     from: account.addr,
     to: newAccount.addr,
     amount: 200_000, // 0.2 ALGO,
+    // Double the fee so newAccount doesn't need to pay a fee
     suggestedParams: { ...suggestedParams, fee: 2 * algosdk.ALGORAND_MIN_TX_FEE, flatFee: true },
   });
 
@@ -120,6 +156,7 @@ async function main() {
     to: newAccount.addr,
     amount: 0,
     assetIndex,
+    // Set fee to zero since account will be paying double fee
     suggestedParams: { ...suggestedParams, fee: 0, flatFee: true },
   });
 
@@ -145,26 +182,34 @@ async function main() {
   const approvalSource = fs.readFileSync('./contract/approval.teal', 'utf8');
   const clearSource = fs.readFileSync('./contract/clear.teal', 'utf8');
 
+  // Compile the TEAL programs
   const approvalCompileResult = await algodClient.compile(approvalSource).do();
   const clearCompileResult = await algodClient.compile(clearSource).do();
 
+  // Convert the compilation result to Uint8Array
   const approvalBytes = new Uint8Array(Buffer.from(approvalCompileResult.result, 'base64'));
   const clearBytes = new Uint8Array(Buffer.from(clearCompileResult.result, 'base64'));
 
+  // Read our ABI JSON file to create an ABIContract object
   const contract = new algosdk.ABIContract(abi);
+
+  // Get the selector for the create method
   const createMethodSelector = algosdk.getMethodByName(contract.methods, 'create').getSelector();
 
   const appCreateTxn = algosdk.makeApplicationCreateTxnFromObject({
     suggestedParams,
-    onComplete: algosdk.OnApplicationComplete.NoOpOC,
+    onComplete: algosdk.OnApplicationComplete.NoOpOC, // Action to take after the appcall passes
     from: account.addr,
-    approvalProgram: approvalBytes,
-    clearProgram: clearBytes,
-    numGlobalByteSlices: 2,
-    numGlobalInts: 1,
-    numLocalByteSlices: 0,
-    numLocalInts: 0,
-    appArgs: [createMethodSelector, (new algosdk.ABIStringType()).encode('Alice')],
+    approvalProgram: approvalBytes, // TEAL program to run on app calls and creation
+    clearProgram: clearBytes, // TEAL program to run when local account state is deleted
+    numGlobalByteSlices: 2, // Number of byteslices stored in global state
+    numGlobalInts: 1, // Number of integers stored in global state
+    numLocalByteSlices: 0, // Number of byteslices stored in local state
+    numLocalInts: 0, // Number of integers stored in local state
+    appArgs: [
+      createMethodSelector, // First argument is always the selector of the method we're calling
+      (new algosdk.ABIStringType()).encode('Alice') // Following arguments are the arguments to the method
+    ],
   });
 
   const signedAppCreateTxn = appCreateTxn.signTxn(account.sk);
@@ -188,9 +233,11 @@ async function main() {
   atc.addMethodCall({
     suggestedParams,
     sender: account.addr,
+    // Signer is a function that takes in unsigned txns and returns signed txns
     signer: async (unsignedTxns) => unsignedTxns.map((t) => t.signTxn(account.sk)),
     appID: appIndex,
     method: algosdk.getMethodByName(contract.methods, 'increment'),
+    // Note how we don't have to manually encode the string
     methodArgs: ['Bob'],
   });
 
