17de00bff6063e4461155416c658317cdfdf1d3c
Joe Polny <joepolny@gmail.com>
2023-07-18 14:06:43 +0200

    add session_2 auction

---
+++ beginner-en/session_2/auction/backend/smart_contracts/auction/contract.py
@@ -0,0 +1,46 @@
+import beaker
+import pyteal as pt
+
+
+class AuctionState:
+    pass
+    ################
+    # Global State #
+    ################
+
+    # Previous bidder: Address of the previous bidder
+
+    # Previous bid: Amount of the previous bid
+
+    # Auction end: Timestamp of the end of the auction
+
+    # ASA amount: Total amount of ASA being auctioned
+
+    # ASA: ID of the ASA being auctioned
+
+    ###############
+    # Local State #
+    ###############
+
+    # Claimable amount: Amount of ALGO this account can reclaim
+
+
+
+app = beaker.Application("Auction", state=AuctionState)
+
+
+# create method that initializes global state
+
+# opt_into_asset method that opts the contract account into an ASA
+
+# start_auction method that starts the auction for a specific length and starting price
+
+# opt_into_app method that allows accounts to opt in to local state
+
+# bid method that allows accounts to bid on the auction
+
+# reclaim_bids method that allows someone to reclaim bids they have previously placed
+
+# claim_asset method that allows the winner to claim the asset
+
+# delete method that allows the owner to delete the contract and retrieve all extra ALGO
