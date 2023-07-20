8756d247b65f19cf676f82bbbe7d6a39f529f9e3
Joe Polny <joepolny@gmail.com>
2023-07-20 08:18:43 +0200

    session_3 progress

--- beginner-en/session_3/auction/frontend/src/components/AppCalls.tsx
+++ beginner-en/session_3/auction/frontend/src/components/AppCalls.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 import * as algokit from '@algorandfoundation/algokit-utils'
 import { useWallet } from '@txnlab/use-wallet'
+import algosdk from 'algosdk'
 import { useSnackbar } from 'notistack'
 import { useState } from 'react'
 import { AuctionState } from '../App'
@@ -52,13 +53,76 @@ const AppCalls = (props: {
         return
       })
 
+      const { appId } = await appClient.appClient.getAppReference()
       setLoading(false)
 
-      const { appId } = await appClient.appClient.getAppReference()
       props.setAppID(Number(appId))
     },
     start: async () => {
       // opt_into_asset and start_auction logic here
+      setLoading(true)
+
+      const assetIndex = (document.getElementById('asa') as HTMLInputElement).valueAsNumber
+      const appAddress = algosdk.getApplicationAddress(props.appID)
+      const suggestedParams = await algodClient.getTransactionParams().do()
+      const atc = new algosdk.AtomicTransactionComposer()
+
+      // opt_into_asset: Pay the application 0.2 ALGO
+      // MBR === Minimum Balance Requirement
+      // 0.1 ALGO for account MBR
+      // 0.1 ALGO for ASA MBR
+      const payment = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+        from: activeAddress!,
+        to: appAddress,
+        amount: 200_000,
+        suggestedParams,
+      })
+
+      atc.addTransaction({ txn: payment, signer })
+
+      // opt_into_asset: call opt_into_asset on the application
+      atc.addMethodCall({
+        method: appClient.appClient.getABIMethod('opt_into_asset')!,
+        methodArgs: [assetIndex],
+        suggestedParams: { ...suggestedParams, fee: 2_000, flatFee: true },
+        sender: sender.addr,
+        signer,
+        appID: props.appID,
+      })
+
+      // start auction
+      const startPrice = (document.getElementById('start') as HTMLInputElement).valueAsNumber
+      const length = (document.getElementById('length') as HTMLInputElement).valueAsNumber
+      const assetAmount = (document.getElementById('asa-amount') as HTMLInputElement).valueAsNumber
+
+      const axfer = algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+        from: activeAddress!,
+        to: appAddress,
+        assetIndex,
+        suggestedParams,
+        amount: assetAmount,
+      })
+
+      atc.addMethodCall({
+        method: appClient.appClient.getABIMethod('start_auction')!,
+        methodArgs: [startPrice, length, { txn: axfer, signer }],
+        suggestedParams,
+        sender: sender.addr,
+        signer,
+        appID: props.appID,
+      })
+
+      try {
+        await atc.execute(algodClient, 3)
+      } catch (e) {
+        console.warn(e)
+        enqueueSnackbar(`Error deploying the contract: ${(e as Error).message}`, { variant: 'error' })
+        setLoading(false)
+        return
+      }
+
+      setLoading(false)
+      props.setAuctionState(AuctionState.Started)
     },
     bid: async () => {
       // bid logic here
@@ -98,11 +162,11 @@ const AppCalls = (props: {
           </label>
           <input type="number" id="asa-amount" defaultValue="0" className="input input-bordered" />
           <label htmlFor="start" className="label m-2">
-            Start Amount
+            Starting Price
           </label>
           <input type="number" id="start" defaultValue="0" className="input input-bordered" />
           <label htmlFor="length" className="label m-2">
-            Length
+            Length (Seconds)
           </label>
           <input type="number" id="length" defaultValue="0" className="input input-bordered" />
           <br />
