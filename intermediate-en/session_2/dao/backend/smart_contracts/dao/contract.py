d5661a4ce4ceeb6f1ebdf654e902e6e4fcf49c0d
Joe Polny <joepolny@gmail.com>
2023-08-01 09:23:12 -0400

    add local

--- intermediate-en/session_2/dao/backend/smart_contracts/dao/contract.py
+++ intermediate-en/session_2/dao/backend/smart_contracts/dao/contract.py
@@ -1,30 +1,31 @@
 import beaker
 import pyteal as pt
 from beaker.lib.storage import BoxMapping
-from typing import Literal
 
-byte32 = pt.abi.StaticArray[pt.abi.Byte, Literal[32]]
-class Proposal(pt.abi.NamedTuple):
-    name: pt.abi.Field[pt.abi.String]
-    unit_name: pt.abi.Field[pt.abi.String]
-    url: pt.abi.Field[pt.abi.String]
-    hash: pt.abi.Field[byte32]
+
+import beaker
+import pyteal as pt
+
+from utils import build
+
 
 class DAOState:
-    proposals = BoxMapping(key_type=pt.abi.Uint64, value_type=Proposal)
+    # Local Storage
+    # Map the proposals to a specific address
 
-    votes = BoxMapping(key_type=pt.abi.Address, value_type=pt.abi.Uint64)
+    # Save the proposal to the specific account
+    proposal = beaker.LocalStateValue(stack_type=pt.TealType.bytes)
 
-    current_proposal_id = beaker.GlobalStateValue(
-        stack_type=pt.TealType.uint64, default=pt.Int(0)
-    )
+    # Votes for the account's proposal
+    votes = beaker.LocalStateValue(stack_type=pt.TealType.uint64)
 
+    # Indicates which proposal currently has the most votes
     winning_proposal = beaker.GlobalStateValue(
         stack_type=pt.TealType.bytes, default=pt.Bytes("")
     )
 
 
-app = beaker.Application("dao", state=DAOState)
+app = beaker.Application("DAO", state=DAOState)
 
 
 @app.create
@@ -32,59 +33,34 @@ def create() -> pt.Expr:
     return app.initialize_global_state()
 
 
-@app.external
-def add_proposal(proposal: Proposal) -> pt.Expr:
-    abi_zero = pt.abi.Uint64()
+# In local state:
+# proposal[txn.sender] = given proposal
+# votes[txn.sender] = 0
+@app.opt_in
+def add_proposal(proposal: pt.abi.String) -> pt.Expr:
     return pt.Seq(
-        abi_zero.set(pt.Int(0)),
-        app.state.proposals[pt.Txn.sender()].set(proposal),
-        app.state.votes[pt.Txn.sender()].set(abi_zero),
+        # Set proposal to given proposal content
+        app.state.proposal[pt.Txn.sender()].set(proposal.get()),
+        # Set votes to zero
+        app.state.votes[pt.Txn.sender()].set(pt.Int(0)),
     )
 
 
 @app.external
-def vote(proposer: pt.abi.Address) -> pt.Expr:
-    abi_current_votes = app.state.votes[proposer]
-    new_votes = pt.Btoi(abi_current_votes.get()) + pt.Int(1)
+def vote(proposer: pt.abi.Account) -> pt.Expr:
+    current_votes = app.state.votes[proposer.address()].get()
+    # Increment votes for given proposal
+    new_votes = current_votes + pt.Int(1)
 
     winning_proposal = app.state.winning_proposal.get()
-    winning_proposal_votes = pt.Btoi(app.state.votes[winning_proposal].get())
-
-    abi_new_votes = pt.abi.Uint64()
+    winning_proposal_votes = app.state.votes[winning_proposal].get()
 
     return pt.Seq(
+        # If new amount of votes for the given proposal is more than the winning proposal
         pt.If(new_votes > winning_proposal_votes).Then(
-            app.state.winning_proposal.set(proposer.get())
+            # Then set the winning proposal to the given proposal
+            app.state.winning_proposal.set(proposer.address())
         ),
-        abi_new_votes.set(new_votes),
-        app.state.votes[proposer].set(abi_new_votes),
-    )
-
-@app.external
-def mint() -> pt.Expr:
-    winning_proposal_id = pt.abi.Uint64()
-    winning_proposal = Proposal()
-
-    url = pt.abi.String()
-    hash = pt.abi.make(byte32)
-    name = pt.abi.String()
-    unit_name = pt.abi.String()
-
-    return pt.Seq(
-        winning_proposal.url.store_into(url),
-        winning_proposal.hash.store_into(hash),
-        winning_proposal.name.store_into(name),
-        winning_proposal.unit_name.store_into(unit_name),
-        winning_proposal_id.set(app.state.winning_proposal.get()),
-        app.state.proposals[winning_proposal_id].store_into(winning_proposal),
-        pt.InnerTxnBuilder.Execute(
-            {
-                pt.TxnField.type_enum: pt.TxnType.AssetConfig,
-                pt.TxnField.config_asset_total: pt.Int(1),
-                pt.TxnField.config_asset_url: url.get(),
-                pt.TxnField.config_asset_metadata_hash: hash.encode(),
-                pt.TxnField.config_asset_name: name.get(),
-                pt.TxnField.config_asset_unit_name: unit_name.get(),
-            }
-        )
+        # Update proposal vote count
+        app.state.votes[proposer.address()].set(new_votes),
     )
