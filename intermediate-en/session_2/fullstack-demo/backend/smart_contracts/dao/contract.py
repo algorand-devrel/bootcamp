7d4c2dec2089661441965dd60b7f71f3eb0cc223
Joe Polny <joepolny@gmail.com>
2023-07-31 20:30:02 -0400

    Proposal/mint

--- intermediate-en/session_2/fullstack-demo/backend/smart_contracts/dao/contract.py
+++ intermediate-en/session_2/fullstack-demo/backend/smart_contracts/dao/contract.py
@@ -1,10 +1,17 @@
 import beaker
 import pyteal as pt
 from beaker.lib.storage import BoxMapping
+from typing import Literal
 
+byte32 = pt.abi.StaticArray[pt.abi.Byte, Literal[32]]
+class Proposal(pt.abi.NamedTuple):
+    name: pt.abi.Field[pt.abi.String]
+    unit_name: pt.abi.Field[pt.abi.String]
+    url: pt.abi.Field[pt.abi.String]
+    hash: pt.abi.Field[byte32]
 
 class DAOState:
-    proposals = BoxMapping(key_type=pt.abi.Uint64, value_type=pt.abi.String)
+    proposals = BoxMapping(key_type=pt.abi.Uint64, value_type=Proposal)
 
     votes = BoxMapping(key_type=pt.abi.Address, value_type=pt.abi.Uint64)
 
@@ -26,7 +33,7 @@ def create() -> pt.Expr:
 
 
 @app.external
-def add_proposal(proposal: pt.abi.String) -> pt.Expr:
+def add_proposal(proposal: Proposal) -> pt.Expr:
     abi_zero = pt.abi.Uint64()
     return pt.Seq(
         abi_zero.set(pt.Int(0)),
@@ -51,4 +58,33 @@ def vote(proposer: pt.abi.Address) -> pt.Expr:
         ),
         abi_new_votes.set(new_votes),
         app.state.votes[proposer].set(abi_new_votes),
+    )
+
+@app.external
+def mint() -> pt.Expr:
+    winning_proposal_id = pt.abi.Uint64()
+    winning_proposal = Proposal()
+
+    url = pt.abi.String()
+    hash = pt.abi.make(byte32)
+    name = pt.abi.String()
+    unit_name = pt.abi.String()
+
+    return pt.Seq(
+        winning_proposal.url.store_into(url),
+        winning_proposal.hash.store_into(hash),
+        winning_proposal.name.store_into(name),
+        winning_proposal.unit_name.store_into(unit_name),
+        winning_proposal_id.set(app.state.winning_proposal.get()),
+        app.state.proposals[winning_proposal_id].store_into(winning_proposal),
+        pt.InnerTxnBuilder.Execute(
+            {
+                pt.TxnField.type_enum: pt.TxnType.AssetConfig,
+                pt.TxnField.config_asset_total: pt.Int(1),
+                pt.TxnField.config_asset_url: url.get(),
+                pt.TxnField.config_asset_metadata_hash: hash.encode(),
+                pt.TxnField.config_asset_name: name.get(),
+                pt.TxnField.config_asset_unit_name: unit_name.get(),
+            }
+        )
     )
