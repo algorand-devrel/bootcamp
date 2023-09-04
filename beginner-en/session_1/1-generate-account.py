71684383325ac25ceee9c87a686e85a73437985d
Ryan R. Fox <ryan@ryanrfox.com>
2023-09-04 00:54:03 -0400

    add templates

---
+++ beginner-en/session_1/1-generate-account.py
@@ -0,0 +1,8 @@
+# don't forget to `pip install algokit-utils` and import as algokit
+
+def main():
+    # use new_account() method to generate a new account
+
+    # print the address of the account object
+
+main()
---
+++ beginner-en/session_1/3-payment-transation.py
@@ -0,0 +1,22 @@
+# imports
+
+def main():
+    # instatiate algod client on localnet
+
+    # instatiate kmd client on localnet
+
+    # create my_account
+
+    # get info about my_account from algod
+
+    #####################################################################
+    # NEW BELOW
+    #####################################################################
+
+    # set localnet default account from KMD as other_account
+
+    # send 1 ALGO from other_account to my_account
+
+    # again, get info about my_account from algod
+ 
+main()
