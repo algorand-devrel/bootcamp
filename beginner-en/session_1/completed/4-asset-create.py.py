0ec4e47df5ba00190c9092f19a8541995a6c6054
Ryan R. Fox <ryan@ryanrfox.com>
2023-09-04 08:38:33 -0400

    add completed firectory

---
+++ beginner-en/session_1/completed/4-asset-create.py.py
@@ -0,0 +1,28 @@
+# add import for algosdk
+
+def main():
+    # use new_account() method to generate a new account
+
+    # instatiate algod client on localnet
+
+    # get info about my_account from algod
+
+    # instatiate kmd client on localnet
+
+    # fund my_account with ALGO from localnet dispenser
+
+    #####################################################################
+    # NEW BELOW
+    #####################################################################
+
+    # create an ASA (Algorand Standard Asset, token, asset, NFT, etc.)
+
+    # sign transaction
+
+    # submit transaction
+ 
+     # again, get info about my_account from algod
+
+    # print the assetID of my new asset from algod
+
+main()
