d829af3b2ff9149b799f5cfaf4070e2a4fc1866b
Ryan R. Fox <ryanRfox@users.noreply.github.com>
2023-09-05 10:05:27 -0400

    Update contract.py

--- beginner-en/session_2/auction/backend/smart_contracts/auction/contract.py
+++ beginner-en/session_2/auction/backend/smart_contracts/auction/contract.py
@@ -3,7 +3,6 @@ import pyteal as pt
 
 
 class AuctionState:
-
     ##################################
     # Global State
     # 64 key-value pairs per contract
@@ -21,12 +20,17 @@ class AuctionState:
     )
     
     # Auction end: Timestamp of the end of the auction
-
+    auction_end = beaker.GlobalStateValue(
+        stack_type=pt.TealType.uint64, default=pt.Int(0)
+    )
+    
     # REMINDER: ASA === Algorand Standard Asset === Asset === Token
 
     # ASA: ID of the ASA being auctioned
+    asa_id = beaker.GlobalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(0))
 
     # ASA amount: Total amount of ASA being auctioned
+    asa = beaker.GlobalStateValue(stack_type=pt.TealType.uint64, default=pt.Int(0))
 
     ##################################
     # Local State
@@ -36,13 +40,19 @@ class AuctionState:
     ##################################
 
     # Claimable amount: Amount of ALGO this account can reclaim
+    claimable_amount = beaker.LocalStateValue(
+        stack_type=pt.TealType.uint64, default=pt.Int(0)
+    )
 
 
 app = beaker.Application("Auction", state=AuctionState)
 
 
 # create method that initializes global state
-
+@app.create(bare=True)
+def create() -> pt.Expr:
+    return.app.initialize_global_state()
+    
 # opt_into_asset method that opts the contract account into an ASA
 
 # start_auction method that starts the auction for a specific length and starting price
